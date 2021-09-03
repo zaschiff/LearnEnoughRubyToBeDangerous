463bf1000d59fe46eaa64f70f5926cfe4dbc581e
Zach Schiff <zaschiff@gmail.com>
2021-09-03 11:26:47 -0600

    completed exercise 4.2.1.2

--- ruby_tutorials/hello_app.rb
+++ ruby_tutorials/hello_app.rb
@@ -1,8 +1,6 @@
 require 'sinatra'
 
 get '/' do
-    DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
-                "Thursday", "Friday", "Saturday"]
-    dayname = DAYNAMES[Time.now.wday]
+    dayname = Date::DAYNAMES[Time.now.wday]
     "Hello, world! Happy #{dayname}."
 end
