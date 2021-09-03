8728121e032d3092310f49745547c71735f29bab
Zach Schiff <zaschiff@gmail.com>
2021-09-03 11:13:54 -0600

    Updated hello world message

--- ruby_tutorials/hello_app.rb
+++ ruby_tutorials/hello_app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
 get '/' do
-    'Goodybye, world!'
+    DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday"]
+    dayname = DAYNAMES[Time.now.wday]
+    "Hello, world! Happy #{dayname}."
 end
